5d04cbc795a99af190ac362be2aa6b692b0b3769
jaiadithya <jaiadithya1400@gmail.com>
2024-07-01 21:35:34 +0530

    contact

--- app/about/page.js
+++ app/about/page.js
@@ -2,6 +2,7 @@
 
 import React from "react";
 import Primarybtn from "../components/button";
+import Image from "next/image";
 
 const page = () => {
   const handleSubmit = () => {
@@ -9,29 +10,49 @@ const page = () => {
   };
 
   return (
-    <div>
-      <div className="px-[200px] ">
-        <div className="space-y-6">
-          <div className="justify-center flex">
-            <h1 className="text-5xl font-black pb-10"> ABOUT </h1>
-          </div>
-
-            <h2 className="text-2xl font-bold">WHAT IS POSTERAXE?</h2>
-            <h3 className="text-lg font-normal">
-              PosterAxe is an art project by Indian designer and artist Jai
-              Adithya. He believes that a poster does not always have to promote
-              an event or product. Instead, Adithya uses the poster medium as a
-              space for expressing his artistic ideas. The main common features of
-              Adithya's work are basic shapes and vivid colors. His work was
-              influenced by the Bauhaus school and a certain nostalgia for and
-              memories of the 90s. Most of the time, Posteraxe's art does not aim
-              to express specific thoughts or deliver a message. The primary goal
-              of the artworks is to visually captivate and please the observer's
-              eye.
-            </h3>
+    <>
+      <div className="space-y-6 mb-20 px-[200px]">
+        <div className="justify-center flex">
+          <h1 className="text-5xl font-black pb-10"> ABOUT </h1>
         </div>
+
+        <h2 className="text-2xl font-bold">WHAT IS POSTERAXE?</h2>
+        <h3 className="text-lg font-normal">
+          PosterAxe is an art project by Indian designer and artist Jai Adithya.
+          He believes that a poster does not always have to promote an event or
+          product. Instead, Adithya uses the poster medium as a space for
+          expressing his artistic ideas. The main common features of Adithya's
+          work are basic shapes and vivid colors. His work was influenced by the
+          Bauhaus school and a certain nostalgia for and memories of the 90s.
+          Most of the time, Posteraxe's art does not aim to express specific
+          thoughts or deliver a message. The primary goal of the artworks is to
+          visually captivate and please the observer's eye.
+        </h3>
+        <div className="relative w-full h-96">
+          <Image
+            src="/images/bgwall.png"
+            alt="Background Wall"
+            layout="fill"
+            objectFit="cover"
+            className="w-full h-full"
+          />
+        </div>
+        <h3 className="text-lg font-normal">
+          The PosterLad project started in 2016. Pecka's style is constantly
+          evolving, and the artist does not stick with one style for too long.
+          Within a few years, PosterLad became popular on social media and was
+          awarded a couple of major international design awards. Pecka is
+          currently a full-time artist and is open to collaborations with
+          clients seeking designs with his personal artistic touch. PosterLad
+          has been recognized in the digital art scene as well. His artworks are
+          presented on the world's most prestigious NFT platform called
+          SuperRare, which hosts only a handful of leading contemporary artists.
+          Pecka has had solo and collective exhibitions on most continents
+          around the world, in places such as Shanghai, Paris, London, or New
+          York. He is represented by Pinzle in the South Korean market.
+        </h3>
       </div>
-    </div>
+    </>
   );
 };
 
--- app/components/navbar.js
+++ app/components/navbar.js
@@ -57,7 +57,7 @@ export default function Navbar() {
             <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
               <li>
                 <a
-                  href="#"
+                  href="/shop"
                   class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 "
                   aria-current="page"
                 >
@@ -74,7 +74,7 @@ export default function Navbar() {
               </li>
               <li>
                 <a
-                  href="#"
+                  href="/news"
                   class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700"
                 >
                   News
@@ -82,7 +82,7 @@ export default function Navbar() {
               </li>
               <li>
                 <a
-                  href="#"
+                  href="/contact"
                   class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700"
                 >
                   Contact
